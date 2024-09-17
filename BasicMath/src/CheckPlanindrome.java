cb6304d09ce2df0df94c9e0a6ecbe3ff89becbee
Rudraansh Bhati <rudaanshbhati.05@gmail.com>
2024-09-17 22:14:51 +0530

    Check Plaindrome method

---
+++ BasicMath/src/CheckPlanindrome.java
@@ -0,0 +1,17 @@
+public class CheckPlanindrome {
+
+        public boolean isPalindrome(int x) {
+            int temp = x;
+            int rev = 0;
+            while(x > 0)
+            {
+                int lastDigit = x % 10;
+                x = x/10;
+                rev = (rev*10) + lastDigit;
+
+            }
+            return temp == rev;
+
+        }
+    }
+
